6cfcc9a632b9131efeece4371dee4ddeb18c6654
praveen <longeu100@gmail.com>
2023-08-24 18:45:03 +0700

    Thêm result và result detail

--- src/main/java/com/kits_internship/edu_flatform/entity/ResultEntity.java
+++ src/main/java/com/kits_internship/edu_flatform/entity/ResultEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.checkerframework.checker.units.qual.C;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.ArrayList;
@@ -16,20 +15,19 @@ import java.util.List;
 @Entity
 @Table(name = "result")
 @RequiredArgsConstructor
-public class ResultEntity extends BaseEntity{
+public class ResultEntity extends BaseEntity {
     @Column(name = "totalPoint")
     private double totalPoint;
-
 
     @Temporal(TemporalType.TIMESTAMP)
     @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private Date createdDate;
 
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL,optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, optional = false)
     @JoinColumn(name = "studentID")
     private StudentEntity student;
 
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL,optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, optional = false)
     @JoinColumn(name = "lectureID")
     private LectureEntity lecture;
 
